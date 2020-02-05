b126179c64874c587fbcfae55601a64255e6cc45
Marco-Longo <marco_longo5@hotmail.it>
2020-02-05 20:18:43 +0000

    Fixed lights and improved boxes movements

--- Puzzle Game/Assets/Scripts/BoxPuzzleTrigger.cs
+++ Puzzle Game/Assets/Scripts/BoxPuzzleTrigger.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BoxPuzzleTrigger : MonoBehaviour
 {
     public GameObject gm;
+    public float force;
 
     void OnTriggerEnter(Collider other)
     {
@@ -21,6 +22,45 @@ public class BoxPuzzleTrigger : MonoBehaviour
         if (other.isTrigger == true && other.CompareTag("boxPuzzleTrigger"))
         {
             gm.GetComponent<GameManager>().DecreasePuzzleCompletion();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //Debug.Log("The player pushed me");
+            Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+
+            //Evaluate hit direction
+            Vector3 direction = (transform.position - collision.gameObject.transform.position).normalized;
+            Ray ray = new Ray(collision.gameObject.transform.position, direction);
+            RaycastHit hit;
+            Physics.Raycast(ray, out hit);
+            Vector3 normal = hit.normal;
+
+            //Apply forces in the correct directions on both the box and the player
+            rb.AddForce(-normal * force, ForceMode.Impulse);
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(normal * force/3, ForceMode.Impulse);
+
+            /*
+            if (normal == transform.forward) //Front Collision
+            {
+                rb.AddForce(Vector3.forward * force, ForceMode.Impulse);
+            }
+            else if (normal == -transform.forward) //Back Collision
+            {
+                
+            }
+            else if (normal == transform.right) //Right Collision
+            {
+                
+            }
+            else if (normal == -transform.right) //Left Collision
+            {
+                
+            }
+            */
         }
     }
 }
